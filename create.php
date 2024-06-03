56479dc95678acb5dadb1a717a5e641f4706f511
Francisco Javier <105751133+Javiervaz10@users.noreply.github.com>
2024-06-03 05:39:55 -0600

    Create create.php

---
+++ create.php
@@ -0,0 +1,23 @@
+<?php
+include 'includes/config.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nombre = $_POST['nombre'];
+    $apellido_paterno = $_POST['apellido_paterno'];
+    $apellido_materno = $_POST['apellido_materno'];
+    $puesto = $_POST['puesto'];
+    $dias_trabajo = $_POST['dias_trabajo'];
+    $horas_trabajo = $_POST['horas_trabajo'];
+    $dias_vacaciones = $_POST['dias_vacaciones'];
+
+    $sql = "INSERT INTO empleados (nombre, apellido_paterno, apellido_materno, puesto, dias_trabajo, horas_trabajo, dias_vacaciones)
+            VALUES ('$nombre', '$apellido_paterno', '$apellido_materno', '$puesto', '$dias_trabajo', '$horas_trabajo', '$dias_vacaciones')";
+
+    if ($conn->query($sql) === TRUE) {
+        header('Location: index.php');
+        exit;
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+?>
