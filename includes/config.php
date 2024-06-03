206f5e7aacc5db681ec385a1cd582a747341e95c
Francisco Javier <105751133+Javiervaz10@users.noreply.github.com>
2024-06-03 05:36:00 -0600

    Create config.php

---
+++ includes/config.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "empresa-db.privatelink.mysql.database.azure.com";
+$username = "u20051268";
+$password = "userL20051268";
+$dbname = "industria";
+$ssl = "./ssl/Microsoft RSA Root Certificate Authority 2017.crt";
+
+// Crear la conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+?>
